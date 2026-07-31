d71831d0432a5228fbda416eeb0a0930e4dba983
Dessert99 <lsjh1234@naver.com>
2026-07-31 23:44:07 +0900

    chore: 스트릭모드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { createRoot } from 'react-dom/client'
 import { App } from './app/App'
 import './styles/global.css'
 
 gsap.registerPlugin(useGSAP)
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
+
 )
